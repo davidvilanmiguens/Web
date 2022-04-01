c2046e5f46c3c79f63f338cf310874ab0faffe14
davidvilanmiguens <72596843+davidvilanmiguens@users.noreply.github.com>
2022-04-01 11:35:17 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,42 @@
+<?php
+get_header()
+?>
+
+<div id = "Hola">
+<h1>Perro ni escribir sabes (-_-)</h1>
+</div>
+
+<div id = "pasa">
+
+
+    <img src="<?php echo get_stylesheet_directory_uri();?>/imagenes/mama.jpg">
+</div>
+
+<div id = "pepe">
+
+
+
+</div>
+
+
+<div id = "contenedor">
+    <div id = "cabecera" class="rojo">
+        <h1>Que tal la Vida bro?</h1>
+    </div>
+    <div id = "cuerpo">
+        <h1></h1>
+
+    </div>
+
+</div>
+
+
+<?php
+get_footer();
+?>
+<div id = "pepe">
+
+<h1>
+<a href = https://s.mxmcdn.net/images-storage/albums5/4/7/1/5/1/7/49715174_350_350.jpg>Solucion para arreglarlo</a>
+</h1>
+</div>
